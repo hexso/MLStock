92325feafa99911b62cfe1f6931c778c88f11311
김준호 <kesu82091@gmail.com>
2023-01-08 19:37:47 +0900

    stock data 받는 함수 구현
    Signed-off-by: 김준호 <kesu82091@gmail.com>

--- stock_data_parse.py
+++ stock_data_parse.py
@@ -1,2 +1,35 @@
 import FinanceDataReader as fdr
+import talib
+import pandas as pd
 
+
+def cal_stock_indicator(data):
+    x = data
+    x.columns = map(str.lower, x.columns)
+    x['obv'] = talib.OBV(x['close'], volume=x['volume'])
+    x = x.dropna()
+    return x
+
+
+def get_stock_data(stock_code: str, indicator=True):
+    data = fdr.DataReader(stock_code)
+    return cal_stock_indicator(data)
+
+
+
+if __name__ == '__main__':
+    data = get_stock_data('005930').dropna()
+    change_func = lambda x: 0 if x < 15 / 100 else 1
+    data['change_over'] = data['change'].apply(change_func)
+    data['change_over_tmw'] = data['change_over'].shift(-1)
+    data['indicator'] = 0
+    print(data)
+    for idx, val in enumerate(data['change_over']):
+        for i in range(10):
+            if idx + i >= len(data):
+                break
+            if data['change_over'][idx + i] == 1:
+                data['indicator'][idx] = 1 - (0.1*idx)
+                break
+
+    print(data)
